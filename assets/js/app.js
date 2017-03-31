be541dd43030d633235734b78410e5ac80f41a1f
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-03-31 14:32:21 -0500

    cambios en js: setAttribute

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,31 +4,23 @@ var select = document.getElementById('select');
 select.onchange = function(){
   if (select.value == 'original') {
     for (var i = 0; i < img.length; i++) {
+      img[i].setAttribute('class', 'animal');
       img[i].classList.add('original');
-      img[i].classList.remove('sepia');
-      img[i].classList.remove('blanco-negro');
-      img[i].classList.remove('invertir-colores');
-    }
+     }
   } else if (select.value == 'sepia') {
       for (var i = 0; i < img.length; i++) {
+        img[i].setAttribute('class', 'animal');
         img[i].classList.add('sepia');
-        img[i].classList.remove('original');
-        img[i].classList.remove('blanco-negro');
-        img[i].classList.remove('invertir-colores');
       }
   } else if (select.value == 'blanco-negro') {
       for (var i = 0; i < img.length; i++) {
+        img[i].setAttribute('class', 'animal');
         img[i].classList.add('blanco-negro');
-        img[i].classList.remove('original');
-        img[i].classList.remove('sepia');
-        img[i].classList.remove('invertir-colores');
       }
   } else if (select.value == 'invertir-colores') {
       for (var i = 0; i < img.length; i++) {
+        img[i].setAttribute('class', 'animal');
         img[i].classList.add('invertir-colores');
-        img[i].classList.remove('original');
-        img[i].classList.remove('sepia');
-        img[i].classList.remove('blanco-negro');
       }
   }
 }
